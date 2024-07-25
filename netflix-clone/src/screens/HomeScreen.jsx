7e7c01561a0f7a6cbba69db8d1603648870763ed
khenya <abrendakhenya@gmail.com>
2024-07-24 20:10:52 -0400

    feat: movie

--- netflix-clone/src/screens/HomeScreen.jsx
+++ netflix-clone/src/screens/HomeScreen.jsx
@@ -1,14 +1,28 @@
-import { SafeAreaView, ScrollView, Text } from 'react-native';
-
+import { SafeAreaView, ScrollView, Text, View, StyleSheet } from 'react-native';
+import { useEffect, useState } from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import CarouselMovie from "../components/common/CarruselMovie";
+import CarouselImage from "../components/common/CarruselImage";
+import { movies } from "../data/movies";
 import { styles } from '../config/theme/app-theme';
 
 const HomeScreen = () => {
-  
+  const [currentMovies, setCurrentMovies] = useState(movies.slice(0, 5));
+  const [allMovies, setAllMovies] = useState(movies.slice(0, 4));
+  const getMoreMovies = (pagination, limit) => {
+    console.log('getMoreMovies');
+    setAllMovies([...allMovies, ...movies.slice(4, 6)]);
+  }
   return (
     <SafeAreaView style={styles.container}>
-        <ScrollView >
-         <Text style = {styles.text}>holas</Text>
-        </ScrollView>
+      <ScrollView >
+        <View>
+          <CarouselMovie movies={allMovies} getMoreMovies={getMoreMovies} />
+        </View>
+        <View>
+          <CarouselImage images={currentMovies} getMoreMovies={getMoreMovies} />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
